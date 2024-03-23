2ec58bf49d3066957cd91a99ba907ddeb5cad9c5
Zoran Pantić <91920971+panticzoran@users.noreply.github.com>
2024-03-23 23:32:31 +0000

    Testing the access working directly from Replit

---
+++ venv/lib/python3.10/site-packages/qiskit_ibm_runtime/batch.py
@@ -0,0 +1,41 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Qiskit Runtime batch mode."""
+
+from typing import Optional, Union
+
+from qiskit.providers.backend import BackendV1, BackendV2
+
+from qiskit_ibm_runtime import QiskitRuntimeService
+from .session import Session
+
+
+class Batch(Session):
+    """Class for creating a batch mode in Qiskit Runtime."""
+
+    def __init__(
+        self,
+        service: Optional[QiskitRuntimeService] = None,
+        backend: Optional[Union[str, BackendV1, BackendV2]] = None,
+        max_time: Optional[Union[int, str]] = None,
+    ):
+        super().__init__(service=service, backend=backend, max_time=max_time)
+
+    def _create_session(self) -> Optional[str]:
+        """Create a session."""
+        if isinstance(self._service, QiskitRuntimeService):
+            session = self._service._api_client.create_session(
+                self.backend(), self._instance, self._max_time, self._service.channel, "batch"
+            )
+            return session.get("id")
+        return None
